467f931ab6a4499235f42e8222816c078d20cadc
unknown <bflana001c@HQSWL-C007359.cable.comcast.com>
2014-11-04 14:37:13 -0500

    Adds flag sprites

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,8 @@ var blogApp = angular.module('blogApp',[]);
 
 blogApp.controller('PostHeroCtrl', function($scope){
     $scope.posts =[
-        {'title': 'Trip', 'imageUrl':'assets/flight.jpg'},
-        {'title': 'Layover', 'imageUrl':'assets/comingsoon.jpg'},
-        {'title': 'Arrival', 'imageUrl':'assets/comingsoon.jpg'}
+        {'title': 'Trip', 'imageUrl':'assets/flight.jpg','flags':['us','ke']},
+        {'title': 'Layover', 'imageUrl':'assets/comingsoon.jpg','flags':['us','ke']},
+        {'title': 'Arrival', 'imageUrl':'assets/comingsoon.jpg','flags':['us','ke']}
     ]
 });
