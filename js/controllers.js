a40df38e720543b6b8867e188a024d36cc943dae
Basil <basil.nyachogo@gmail.com>
2014-11-05 09:35:00 -0500

    making flag selection dynamic

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,8 @@ var blogApp = angular.module('blogApp',[]);
 
 blogApp.controller('PostHeroCtrl', function($scope){
     $scope.posts =[
-        {'title': 'Trip', 'imageUrl':'assets/flight.jpg','flags':['us','ke']},
-        {'title': 'Layover', 'imageUrl':'assets/comingsoon.jpg','flags':['us','ke']},
-        {'title': 'Arrival', 'imageUrl':'assets/comingsoon.jpg','flags':['us','ke']}
+        {'title': 'Trip', 'imageUrl':'assets/flight.jpg','flags':[{'css':'flag flag-us','alt':'us'},{'css':'flag flag-ke','alt':'ke'}]},
+        {'title': 'Layover', 'imageUrl':'assets/comingsoon.jpg','flags':[{'css':'flag flag-us','alt':'us'},{'css':'flag flag-ke','alt':'ke'}]},
+        {'title': 'Arrival', 'imageUrl':'assets/comingsoon.jpg','flags':[{'css':'flag flag-us','alt':'us'},{'css':'flag flag-ke','alt':'ke'}]}
     ]
 });
